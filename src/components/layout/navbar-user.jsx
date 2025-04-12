09a31687489a86903b51c81e9c658f2523070aaa
irwanadinata <irwanadinata9@gmail.com>
2025-04-12 13:00:07 +0700

    chore: integration api dashboard user

--- src/components/layout/navbar-user.jsx
+++ src/components/layout/navbar-user.jsx
@@ -1,26 +1,64 @@
 import logotelu1 from "@/assets/logo/telu-logo1.png";
+import Swal from "sweetalert2";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { getProfile } from "@/utils/api/dashboard";
 import { LogOut } from "lucide-react";
 import { ChevronDown } from "lucide-react";
 import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/utils/context/auth-context"; 
 
 const NavbarUser = () => {
-  const [name, setName] = useState("Irwan Adinata");
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+  const {logout } = useAuth();
+
+  const Toast = Swal.mixin({
+    toast: true,
+    position: "top-end",
+    showConfirmButton: false,
+    timer: 3000,
+    didOpen: (toast) => {
+      toast.onmouseenter = Swal.stopTimer;
+      toast.onmouseleave = Swal.resumeTimer;
+    },
+  });
 
   // Fungsi untuk membatasi maksimal kata nama
   const truncateText = (text, maxChars) => {
+    if (!text) return "";
     if (text.length > maxChars) {
       return text.slice(0, maxChars);
     }
     return text;
   };
+  
+  const fetchName = async () => {
+    try {
+      const response = await getProfile();
+      setName(response.nama);
+    } catch (error) {
+      console.error("Gagal mengambil profil user:", error);
+    }
+  };
+  
+  const handleLogout = () => {
+    logout();
+    Toast.fire({
+      icon: "success",
+      title: "Logout berhasil!",
+    });
+    navigate("/");
+  };
 
-  useEffect(() => {});
+  useEffect(() => {
+    fetchName();
+  }, []);
 
   return (
     <div className="bg-gradient-to-l from-[#bf131d] via-[#bf131d] to-[#580c0e] py-4">
@@ -53,9 +91,9 @@ const NavbarUser = () => {
             </DropdownMenuTrigger>
 
             <DropdownMenuContent>
-              <div className="px-3 py-2 text-[#F64C4C] cursor-pointer flex gap-2">
+              <div className="px-3 py-2 text-[#F64C4C] cursor-pointer flex gap-2" >
                 <LogOut className="w-5 h-5" />
-                <p className="mt-[-0.125rem]">Keluar</p>
+                <p className="mt-[-0.125rem]" onClick={handleLogout}>Keluar</p>
               </div>
             </DropdownMenuContent>
           </DropdownMenu>
