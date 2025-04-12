f2b4d19921720c307c353f2e15c77f2c860d182d
irwanadinata <irwanadinata9@gmail.com>
2025-04-12 12:56:57 +0700

    refactor: input file component

--- src/components/ui/input-file.jsx
+++ src/components/ui/input-file.jsx
@@ -4,28 +4,20 @@ import placeholder from "@/assets/icon/foto-icon.svg";
 function FileInput({
   onChange,
   id,
-  preview,
   disabled,
   description = "Tambahkan file di sini",
 }) {
   return (
     <div
-      className={`relative h-28 rounded-lg border border-input flex items-center ${
-        preview ? "justify-start" : "justify-center"
-      }`}
+      className={`relative h-14 rounded-lg border border-input flex items-center justify-center
+      `}
     >
-      {preview ? (
-        <div className="absolute flex items-end gap-3">
-          <img src={preview} className="h-24 rounded-lg ms-2 border" />
+      <div className="absolute">
+        <div className="flex gap-2 items-center">
+          <img src={placeholder} className="w-8 h-8" />
+          <span className="block text-gray-400 font-normal">{description}</span>
         </div>
-      ) : (
-        <div className="absolute">
-          <div className="flex gap-2 items-center">
-            <img src={placeholder} className="w-8 h-8" />
-            <span className="block text-gray-400 font-normal">{description}</span>
-          </div>
-        </div>
-      )}
+      </div>
       <Input
         id={id}
         type="file"
