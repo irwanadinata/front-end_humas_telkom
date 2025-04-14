6ff4a17e8b111024b918799510fa8ed9660663ca
irwanadinata <irwanadinata9@gmail.com>
2025-04-15 02:12:20 +0700

    refactor: api auth

--- src/utils/api/auth/api.js
+++ src/utils/api/auth/api.js
@@ -6,7 +6,6 @@ export const login = async (email, password) => {
       email,
       password,
     });
-    console.log(response)
     return response.data;
   } catch (error) {
     throw new Error("Email atau kata sandi salah");
